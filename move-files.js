afd2812649b567772f236c119775e12d915b8563
isfelaco <isfelaco@gmail.com>
2024-02-15 09:37:19 -0500

    working deployment

--- move-files.js
+++ move-files.js
@@ -1,8 +1,20 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 
+const staticDir = path.join(__dirname, "..", "isabellafelaco.com", "static");
+
+// Delete the 'C:\Portfolio\static' directory
+try {
+  fs.removeSync(staticDir);
+  console.log(
+    "Directory 'C:\\isabellafelaco.com\\static' deleted successfully."
+  );
+} catch (err) {
+  console.error("Error deleting directory:", err);
+}
+
 const sourceDir = path.join(__dirname, "build");
-const destDir = path.join(__dirname, "..", "Portfolio");
+const destDir = path.join(__dirname, "..", "isabellafelaco.com");
 
 fs.readdirSync(sourceDir).forEach((file) => {
   const sourceFile = path.join(sourceDir, file);
