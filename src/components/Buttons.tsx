c9630fd7c68a6fec53659ed9abf1f30887d62477
isfelaco <isfelaco@gmail.com>
2024-03-02 21:32:14 -0500

    updating button hover

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -3,68 +3,72 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 const WhiteButton = styled.button<{ float?: string | undefined }>`
-	background-color: rgb(255, 255, 255, 0.5);
-	border: 0.25px solid white;
-	border-radius: 5px;
-	height: 50px;
-	width: 100px;
-	cursor: pointer;
-	float: ${({ float }) => float};
+  background-color: rgb(255, 255, 255, 0.5);
+  border: 0.25px solid white;
+  border-radius: 5px;
+  height: 50px;
+  width: 100px;
+  cursor: pointer;
+  float: ${({ float }) => float};
 
-	&:hover {
-		border: 0.5px solid white;
-	}
+  &:hover {
+    border: 0.25px solid gray;
+  }
 
-	color: #aa4586;
-	font-family: myFont;
-	font-weight: bold;
-	font-size: 20px;
-	text-transform: uppercase;
+  color: #aa4586;
+  font-family: myFont;
+  font-weight: bold;
+  font-size: 20px;
+  text-transform: uppercase;
 `;
 
 export const PinkButton = styled.button`
-	border: 1px solid white;
-	border-radius: 5px;
-	padding: 5px 10px 5px 10px;
-	color: white;
-	background: #c46ea6;
-	width: 200px;
-	cursor: pointer;
-	color: white;
+  border: 1px solid white;
+  border-radius: 5px;
+  padding: 5px 10px 5px 10px;
+  color: white;
+  background: #c46ea6;
+  width: 200px;
+  cursor: pointer;
+  color: white;
+
+  &:hover {
+    border: 1px solid gray;
+  }
 `;
 
 type ButtonProps = {
-	style: string;
-	to: string;
-	text: string;
-	float?: string;
+  style: string;
+  to: string;
+  text: string;
+  float?: string;
 };
 
 export function Button({ style, to, text, float }: ButtonProps) {
-	return (
-		<div>
-			<Link to={to}>
-				{style === "white" ? (
-					<WhiteButton float={float}>{text}</WhiteButton>
-				) : (
-					<PinkButton>{text}</PinkButton>
-				)}
-			</Link>
-		</div>
-	);
+  return (
+    <div>
+      <Link to={to}>
+        {style === "white" ? (
+          <WhiteButton float={float}>{text}</WhiteButton>
+        ) : (
+          <PinkButton>{text}</PinkButton>
+        )}
+      </Link>
+    </div>
+  );
 }
 
 const ButtonContainer = styled.div<{ align?: string | undefined }>`
-	display: flex;
-	gap: 20px;
-	justify-content: ${({ align }) => align};
+  display: flex;
+  gap: 20px;
+  justify-content: ${({ align }) => align};
 `;
 
 type ButtonRowProps = {
-	children: ReactElement[];
-	align?: string;
+  children: ReactElement[];
+  align?: string;
 };
 
 export function ButtonRow({ children, align }: ButtonRowProps) {
-	return <ButtonContainer align={align}>{children}</ButtonContainer>;
+  return <ButtonContainer align={align}>{children}</ButtonContainer>;
 }
