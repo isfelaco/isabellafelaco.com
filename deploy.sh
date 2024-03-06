97bdb81a2a5ee91a3f5095f32bcbff48a258d1ed
isfelaco <isfelaco@gmail.com>
2024-03-06 12:23:44 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 # Change to your web directory
-# cd isabellafelaco.com
+cd isabellafelaco.com
 
 # Pull changes from GitHub repository
 git pull origin main
 
-# Optionally, install dependencies and build assets
-# yarn install
-
-# Restart services if necessary
-# systemctl restart your_service_name
-
-# Optionally, set permissions
-# chmod -R 755 /path/to/your/web/directory
-
 echo "Deployment completed successfully"
+
