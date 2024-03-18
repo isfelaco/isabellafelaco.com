386a7cdf183a4281c1b49e3763a1d7fd28b346be
isfelaco <isfelaco@gmail.com>
2024-03-18 18:15:11 -0400

    reorganizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import "./App.css";
+import "./styles/App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
